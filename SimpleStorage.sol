f48425717455db41fd5c3086608f2774e4b6108f
vahidabolhassani2 <vahid.abolhassani2@gmail.com>
2025-07-30 12:19:48 +0330

    c4

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.7;
- contract SimpleStorage {
+
+contract SimpleStorage {
     function addmod() public nonpayable returns (uint256) {
-        
-        return  2;
+        uint256 a = 2;
+        return 2;
     }
-    
- }
+}
